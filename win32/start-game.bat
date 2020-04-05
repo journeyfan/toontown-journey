44566086aa0ebbf8df515722e52bfa8e013c340f
Chris Darch <comradenapcal@aol.com>
2020-04-04 20:19:46 -0700

    ttj: Added client start batch script.

---
+++ win32/start-game.bat
@@ -0,0 +1,13 @@
+@echo off
+cd ../../
+
+title Toontown Game Launcher
+
+set TTI_GAMESERVER=127.0.0.1
+set /P TT_PLAYCOOKIE=Username:
+
+echo Starting Toontown...
+
+:main
+C:/Panda3D-1.11.0-x64-py2/python/python.exe -m toontown.toonbase.ToontownStart
+goto main
