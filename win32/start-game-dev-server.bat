faa0a0dfa6bd02f49a216bde3dbe0b6250e29040
Chris Darch <comradenapcal@aol.com>
2020-04-05 21:53:27 -0700

    launcher: Fix this time.

--- win32/start-game-dev-server.bat
+++ win32/start-game-dev-server.bat
@@ -4,9 +4,10 @@ cd ../
 title Toontown Game Launcher
 
 set TTI_GAMESERVER=192.99.167.158
-set /P username=Username:
-set /P password=Password:
-set TTI_PLAYCOOKIE = %username%:%password%
+set /P username="Username:"
+set /P password="Password:"
+set TTI_PLAYCOOKIE=%username%:%password%
+echo %TTI_PLAYCOOKIE%
 echo Starting Toontown...
 
 :main
